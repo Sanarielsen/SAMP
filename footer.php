f650670b4c5a9cb020ff16d190403b4c88c61f7e
Sanarielsen <sanarielsen.dev@gmail.com>
2026-04-24 17:10:43 -0300

    chore: delete the old version of SAMP
    delete all system to prepare the structure to built the new version of
    software with React and Node

--- footer.php
+++
@@ -1,8 +0,0 @@
-<!-- Encerramento da div que irá conter o menu lateral e o conteudo do sistema -->
-</div>			
-<!-- Encerramento da div que irá cobrir toda a tela em questão de largura -->
-</div>
-<!-- Encerramento da tag de body -->
-</body>
-<!-- Encerramento da tag de html -->
-</html>
--- identificacao_outline.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	session_start();
-
-	unset($_SESSION['userOnline']);
-
-	echo "<script> window.location='index.php' </script>";
-	
-?>
