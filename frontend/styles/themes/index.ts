2d5ae4aff7df8235e1d45496392c77cdfc656bd4
Sanarielsen <sanarielsen.dev@gmail.com>
2026-04-27 11:28:50 -0300

    feat(SMP-01): install and setup many libs
    Install and setup many things to start a develop this application, like:
    - react-query
    - react-dom
    - react-router
    - MUI and styled components
    - default configuration about base paths
    
    And added the base tokens to use in a develop.

---
+++ frontend/styles/themes/index.ts
@@ -0,0 +1,81 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      light: '#973C00',
+      main: '#973C00',
+      dark: '#973C00',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#894B00',
+      main: '#fff',
+      dark: '#894B00',
+      contrastText: '#fff',
+    },
+    background: {
+      default: '#F5F5F4',
+    },
+    success: {
+      main: '#016630',
+    },
+    error: {
+      main: '#9F0712',
+    },
+    warning: {
+      main: '#F0B100',
+    },
+    info: {
+      main: '#00598A',
+    },
+    common: {
+      white: '#F5F5F4',
+      black: '#000000',
+    },
+    grey: {
+      800: '#ABA09C' 
+    }
+  },
+});
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      light: '#973C00',
+      main: '#973C00',
+      dark: '#973C00',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#894B00',
+      main: '#fff',
+      dark: '#894B00',
+      contrastText: '#fff',
+    },
+    background: {
+      default: '#F5F5F4',
+    },
+    success: {
+      main: '#016630',
+    },
+    error: {
+      main: '#9F0712',
+    },
+    warning: {
+      main: '#F0B100',
+    },
+    info: {
+      main: '#00598A',
+    },
+    common: {
+      white: '#F5F5F4',
+      black: '#000000',
+    },
+    grey: {
+      800: '#ABA09C' 
+    }
+  },
+});
