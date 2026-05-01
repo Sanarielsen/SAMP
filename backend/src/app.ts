e79c0f4efc65cc0eb65332d7d93e65781379ab22
Sanarielsen <sanarielsen.dev@gmail.com>
2026-05-01 12:08:29 -0300

    feat(SMP-03): create user controller and services
    Create user controller to manage all things about route and services
    respectively to read all information about database, prisma or other
    validation.
    
    Resolves: SMP-03-02

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,24 @@
 import fastify from "fastify";
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from "@/http/routes";
+import { env } from "./env";
+import { ZodError } from "zod";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.register(appRoutes);
 
-prisma.user.create({
-  data: {
-    name: 'Samuel Henrique',
-    email: 'sanarielsen.dev@gmail.com'
-    
+app.setErrorHandler( ( error, _, reply) => {
+  if ( error instanceof ZodError ) {
+    return reply.status(400).send({
+      message: 'Validation error.', issues: error.format()
+    })
   }
-})
 
+  if ( env.NODE_ENV !== 'production' ) {
+    console.log(error)
+  } else {
+    //Todo: Here we should log to an external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+} )
---
+++ backend/src/http/Controllers/register.ts
@@ -0,0 +1,34 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { RegisterUseCase } from '@/services/register'
+import { UserAlreadyExistsError } from '@/services/errors/user-already-exists'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string().min(6)
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
+
+    await registerUseCase.execute({
+      name,
+      email,
+      password
+    })
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+
+    throw err
+  }
+  return reply.status(201).send();
+}
