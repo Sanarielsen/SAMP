e60d61195ad63fad3cd37c3832265712557262a3
Sanarielsen <sanarielsen.dev@gmail.com>
2026-05-05 18:53:50 -0300

    feat(SMP-05): create dashbord, menu and header
    Create a header menu can be used to navigate on the dashboard.
    
    Create side menu to navigate between the pages I will develop on this
    application.
    
    Its important to know we have a correct way to work with MUI here,
    but I build just the first vision of this menu, but its important I
    have other things to do here, like a visual on Desktop and use
    components of MUI, not a custom components I build
    
    Create base pages to navigate all section will be developed as soon
    as well. Created a skeleton to clients, representatives and about.
    
    Change the contrast color when I have primary color on pallete.
    
    I removed the "ProtectedRoute", because I not use it anymore

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router";
+
+import AboutPage from "@/features/about/pages/AboutPage";
+import ClientPage from "@/features/client/pages/ClientPage";
+import Dashboard from "@/layout/Dashboard";
+import ForgotPasswordPage from "@/features/auth/pages/ForgotPassword";
+import LoginPage from "@/features/auth/pages/Login";
+import RepresentativePage from "@/features/representative/pages/RepresentativePage";
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route index element={<LoginPage />} />
+      <Route path="recuperar-senha" element={<ForgotPasswordPage />} />
+
+      <Route element={<Dashboard />}>
+        <Route path="clientes" element={<ClientPage />} />
+        <Route path="representantes" element={<RepresentativePage />} />
+        <Route path="sobre" element={<AboutPage />} />
+      </Route>
+    </Routes>
+  )
+}
