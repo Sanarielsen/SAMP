5ee116dfb455331eeb4bdc186dae29a2b9b32ecb
Sanarielsen <sanarielsen.dev@gmail.com>
2026-05-05 18:53:50 -0300

    feat(SMP-05): create dashbord, menu and header
    Create a header menu can be used to navigate on the dashboard.
    
    Create side menu to navigate between the pages I will develop on this
    application.
    
    Its important to know we have a correct way to work with MUI here,
    but I build just the first vision of this menu, but its important I
    have other things to do here, like a visual on Desktop and use
    components of MUI, not a custom components I build
    
    Create base pages to navigate all section will be developed as soon
    as well. Created a skeleton to clients, representatives and about.
    
    Change the contrast color when I have primary color on pallete.
    
    I removed the "ProtectedRoute", because I not use it anymore

---
+++ frontend/src/components/SideMenu.tsx
@@ -0,0 +1,66 @@
+import { useNavigate, useLocation } from "react-router";
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import CloseIcon from '@mui/icons-material/Close';
+import HotelClassIcon from '@mui/icons-material/HotelClass';
+import InfoIcon from '@mui/icons-material/Info';
+
+import { 
+  ContainerMenu,
+  MenuItem,
+  NavBody,
+  NavClose,
+  NavHeader 
+} from "@/styles/menuContainer";
+
+interface SideMenuProps {
+  open: boolean;
+  handleChangeStatus: () => void;
+}
+
+export default function SideMenu({ open, handleChangeStatus }: SideMenuProps) {
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  function handleChangePage(targetPage: string) {
+    navigate(targetPage);
+    handleChangeStatus();
+  }
+
+  return (
+    <ContainerMenu $active={open}>
+      <NavClose onClick={ () => handleChangeStatus() }>
+        <CloseIcon />
+      </NavClose>
+      <NavHeader>
+        <AccountCircleIcon color='inherit' fontSize='large' />
+        <p> Seja bem-vindo, Samuel Henrique </p>
+      </NavHeader>
+      <NavBody>
+        <MenuItem
+          $active={location.pathname === "/clientes"}
+          onClick={() => handleChangePage("/clientes")}
+        >
+          <ApartmentIcon />
+          <span> Clientes </span>
+        </MenuItem>
+        <MenuItem
+          $active={location.pathname === "/representantes"}
+          onClick={() => handleChangePage("/representantes")}
+        >
+          <HotelClassIcon />
+          <span> Representantes </span>
+        </MenuItem>
+        <MenuItem
+          $active={location.pathname === "/sobre"}
+          onClick={() => handleChangePage("/sobre")}
+        >
+          <InfoIcon />
+          <span> Sobre </span>
+        </MenuItem>
+      </NavBody>
+    </ContainerMenu>
+  )
+}
