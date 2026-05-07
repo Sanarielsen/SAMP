626e76f67eca005e25c07c36c2cbe9cb2f1a472d
Sanarielsen <sanarielsen.dev@gmail.com>
2026-05-07 11:26:22 -0300

    feat(SMP-06): create page and other components about client
    Create page with two components would be added on other pages: DataTable
    and SearchInput. This part I focus only an struture of this.

---
+++ frontend/src/features/client/utils/mockConstants.ts
@@ -0,0 +1,11 @@
+//Mock List Clients => /clients
+export const MOCK_CLIENTS = [
+  { id: 1, name: "Samuel Henrique" },
+  { id: 2, name: "Abilio Correa" },
+  { id: 3, name: "Alves Junior" },
+  { id: 4, name: "Danillo Bosco" },
+  { id: 5, name: "Monica Craveiro" },
+  { id: 6, name: "Pachi Parra" },
+  { id: 7, name: "Laisa Grijo" },
+];
+
