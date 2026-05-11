916ee4d90fdd2172b3a3d7b3a7a36ae39d65db0b
Sanarielsen <sanarielsen.dev@gmail.com>
2026-05-11 15:21:13 -0300

    feat(SMP-07): create profile logic

--- backend/src/http/routes.ts
+++ backend/src/http/routes.ts
@@ -1,11 +1,15 @@
 import { FastifyInstance } from "fastify";
 
 import { authenticate } from "@/http/Controllers/authenticate";
+import { profile } from "@/http/Controllers/profile";
 import { register } from "@/http/Controllers/register";
+import { verifyJWT } from "./middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
 
   app.post('/users', register)
 
   app.post('/session', authenticate)
+
+  app.get('/me', {onRequest: [verifyJWT]}, profile)
 }
