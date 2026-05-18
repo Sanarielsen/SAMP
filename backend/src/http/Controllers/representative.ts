e996cea5d7bdca6b41fb35ea394a0f49623e0f27
Sanarielsen <sanarielsen.dev@gmail.com>
2026-05-18 15:14:26 -0300

    feat(SMP-09): implement representative create new representive route
    Create the route responsible for listing representatives
    assigned to the logged-in user.

--- backend/src/http/Controllers/representative.ts
+++ backend/src/http/Controllers/representative.ts
@@ -1,6 +1,8 @@
 import { ResourceNotFoundError } from "@/services/errors/resource-not-found-error";
 import { makeListRepresentativeUseCase } from "@/services/factories/representatives/make-list-use-case";
+import { makePostRepresentativeUseCase } from "@/services/factories/representatives/make-post-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
+import { z, ZodError } from "zod";
 
 
 export async function listRepresentative(request: FastifyRequest, reply: FastifyReply) {
@@ -22,5 +24,52 @@ export async function listRepresentative(request: FastifyRequest, reply: Fastify
         message: err.message,
       })
     }
+  }
+}
+
+export async function postRepresentative(request: FastifyRequest, reply: FastifyReply) {
+
+  const createRepresentativeBodySchema = z.object({
+    name: z.string().min(1),
+    nacionality: z.string().min(1),
+    documentRG: z.string().min(8).max(9),
+    documentCPF: z.string().min(11).max(12),
+    titleJob: z.string().min(1),
+    roleJob: z.string().min(1)
+  })
+
+  const {
+    name,
+    nacionality,
+    documentRG,
+    documentCPF,
+    titleJob,
+    roleJob
+  } = createRepresentativeBodySchema.parse(request.body)
+
+  const postRepresentativeUseCase = makePostRepresentativeUseCase();
+
+  const { id } = request.params as { id: string }
+
+  try {
+    await postRepresentativeUseCase.execute({
+      idClient: id,
+      name,
+      nacionality,
+      documentRG,
+      documentCPF,
+      titleJob, 
+      roleJob,
+      createdAt: new Date(Date.now())
+    })
+  } catch (err) {
+    if (err instanceof ZodError) {
+      return reply.status(400).send({
+        message: 'Validation error.',
+        issues: err.flatten().fieldErrors,
+      })
+    }
+
+    throw err;
   }
 }
