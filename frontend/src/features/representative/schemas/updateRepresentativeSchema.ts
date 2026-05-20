bf6f1b611a7c72a817c0f81cf9222d52b0687214
Sanarielsen <sanarielsen.dev@gmail.com>
2026-05-20 15:52:02 -0300

    feat(SMP-10): create skeleton of manage representative
    Create the initial components and page to receive add or update a
    representative already registered.
    
    I make in a fix around ControlledComboBox to receive correctly the value
    I put on the schema.

---
+++ frontend/src/features/representative/schemas/updateRepresentativeSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const manageRepresentativeSchema = z.object({
+
+  idClient: z.string({error: 'Informe a empresa que esse representante responde.'}),
+  name: z.string({error: 'Informe o nome do representante.'}).trim(),
+  nationality: z.string({error: 'Informe a nacionalidade do representante.'}).trim(),
+  documentRG: z.string({error: 'Informe o RG do representante.'}).trim(),
+  documentCPF: z.string({error: 'Informe o CPF do representante.'}).trim(),
+  titleJob: z.string({error: 'Informe a profissao deste representante.'}),
+  roleJob: z.string({error: 'Informe o cargo do representante.'}).trim()
+});
+
+export type ManageRepresentativeSchemaFormData =
+  z.infer<typeof manageRepresentativeSchema>;
