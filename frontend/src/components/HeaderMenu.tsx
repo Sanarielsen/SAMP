014507b09d798aa9df8cf8b0001692d55d27a87b
Sanarielsen <sanarielsen.dev@gmail.com>
2026-06-01 15:09:08 -0300

    feat(SMP-13): create base layout for menu header
    Create the initial popover layout to display all available system
    actions. At this stage, the menu is shown regardless of the currently
    logged-in user's role.

--- frontend/src/components/HeaderMenu.tsx
+++ frontend/src/components/HeaderMenu.tsx
@@ -2,15 +2,25 @@ import { useState } from 'react';
 
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Tooltip } from '@mui/material';
+import { Popover, Tooltip } from '@mui/material';
 
 import { ButtonIcon, ContainerHeader } from '@/styles/headerContainer';
 import SideMenu from '@/components/SideMenu';
+import MenuProfile from './MenuProfile';
 
 
 export default function HeaderMenu() {
 
   const [active, setActive] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+
+  const handleProfileClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleProfileClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <>
@@ -27,10 +37,20 @@ export default function HeaderMenu() {
           </ButtonIcon>
         </Tooltip>
         <Tooltip title="Perfil">
-          <ButtonIcon>
+          <ButtonIcon onClick={handleProfileClick}>
             <AccountCircleIcon color='inherit' fontSize='large' />
           </ButtonIcon>
         </Tooltip>
+
+        <Popover
+          open={Boolean(anchorEl)}
+          anchorEl={anchorEl}
+          onClose={handleProfileClose}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        >
+          <MenuProfile />
+        </Popover>
       </ContainerHeader>
     </>
   )
