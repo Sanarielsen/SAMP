1c04067db5e3a090d0f1fc829863140f662041f7
Sanarielsen <sanarielsen.dev@gmail.com>
2026-06-16 19:35:42 -0300

    feat(SMP-14): create route to list payment methods
    Create the route and tests to retrieve all active payment methods.

---
+++ backend/src/repositories/in-memory/in-memory-payment-methods-repository.ts
@@ -0,0 +1,53 @@
+import { CreatePaymentMethodDTO, PaymentMethod } from "@shared/types/paymentMethod";
+import { PaymentMethodRepository } from "../payment-method-repository";
+
+
+export class InMemoryPaymentMethodsRepository implements PaymentMethodRepository {
+  public items: PaymentMethod[] = []
+
+  async create(data: CreatePaymentMethodDTO): Promise<PaymentMethod> {
+        
+    const paymentMethod: PaymentMethod = {
+      ...data,
+      id: crypto.randomUUID(),
+      observation: data.observation ?? null,
+      createdAt: new Date(Date.now()),
+      updatedAt: null,
+      deletedAt: null
+    }
+
+    this.items.push(paymentMethod)
+
+    return paymentMethod
+  }
+
+  async delete(id: string): Promise<PaymentMethod> {    
+    const paymentMethodIndex = this.items.findIndex(method => {
+      return method.id === id
+    })
+
+    const disabledPaymentMethod = {
+      ...this.items[paymentMethodIndex],
+      deletedAt: new Date(),
+    }
+
+    this.items[paymentMethodIndex] = disabledPaymentMethod
+
+    return disabledPaymentMethod
+      
+  }
+  
+  async findById(id: string): Promise<PaymentMethod | null> {
+    const paymentMethod = this.items.find(method => method.id == id)
+
+    if (!paymentMethod) {
+      return null
+    }
+    
+    return paymentMethod
+  }
+
+  async findManyActive(): Promise<PaymentMethod[]> {
+    return this.items.filter(methods => methods.deletedAt === null)
+  }
+}
