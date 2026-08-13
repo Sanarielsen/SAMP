28bc2d5da2019a3c21107394a6d205594cf02ce3
Samuel Aka Sanarielsen <sanarielsen.dev@gmail.com>
2026-08-13 17:58:26 -0300

    feat(SMP-20): create process publication delete

--- backend/src/repositories/process-publication-repository.ts
+++ backend/src/repositories/process-publication-repository.ts
@@ -10,6 +10,7 @@ export interface ProcessPublicationRepository {
   create(data: ProcessPublicationCreateDTO): Promise<ProcessPublication>
   createMany(data: ProcessPublicationCreateFromINPIDTO): Promise<number>
   update(data: Partial<ProcessPublicationUpdateDTO>): Promise<ProcessPublication>
+  delete(id: string): Promise<void>
 
   findById(id: string): Promise<ProcessPublication | null>
   findManyByProcessId(processId: string): Promise<ProcessPublication[]>
